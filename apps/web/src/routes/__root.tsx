1803e16bc6ef3e6b8d2272b0162bd82d7a860ee5
Lukelin <867976148@qq.com>
2026-08-10 19:19:10 +0800

    修改浏览器标签页

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -50,8 +50,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 		return {
 			links: [
 				// Icons
-				{ rel: "icon", href: "/favicon.ico", type: "image/x-icon", sizes: "128x128" },
-				{ rel: "icon", href: "/favicon.svg", type: "image/svg+xml", sizes: "256x256 any" },
+				{ rel: "icon", href: "/logo/light.svg", type: "image/svg+xml" },
 				{ rel: "apple-touch-icon", href: "/apple-touch-icon-180x180.png", type: "image/png", sizes: "180x180 any" },
 				// Manifest
 				{ rel: "manifest", href: "/manifest.webmanifest", crossOrigin: "use-credentials" },
